7e3d2569c6e0af6caab601590a46dc0e023e9d08
Ducbii4202 <144377938+Ducbii4202@users.noreply.github.com>
2025-04-20 22:23:04 +0700

    fix distance

--- .history/src/App_20250420220528.jsx
+++ .history/src/App_20250420220528.jsx
@@ -5,7 +5,7 @@ import Projects from './components/Projects';
 import About from './components/About';
 import Experience from './components/Experience';
 import Skill from './components/Skill';
-
+// import Contact from './components/Contact';
 import Hero1 from './components/Hero1';
 import Contact1 from './components/Contact1';
 
@@ -18,9 +18,12 @@ const App = () => {
                 <Navbar />
                 <Hero1 />
                 <About />
+
                 <Projects />
+
                 <Experience />
                 <Skill />
+
                 <Contact1 />
             </main>
         </>
