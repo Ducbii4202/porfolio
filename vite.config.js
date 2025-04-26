77e37133f09c054d28d1bfc59673d31b28580707
DatVX02 <vxdat13@gmail.com>
2025-04-26 15:35:00 +0700

    ..

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    base: '/porfolio/', // tên repo của bạn
+    base: '/porfolio/',
     plugins: [react(), tailwindcss()]
 });
