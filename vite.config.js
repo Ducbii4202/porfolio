ace242dbe4f3fc74de604b643a150bc3bb7911bc
DatVX02 <vxdat13@gmail.com>
2025-04-26 15:52:57 +0700

    ././/

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    base: '/porfolio/',
-    plugins: [react()],
+    base: './', // ✅ base local
+    plugins: [react(), tailwindcss()],
     build: {
         outDir: 'dist'
     }
